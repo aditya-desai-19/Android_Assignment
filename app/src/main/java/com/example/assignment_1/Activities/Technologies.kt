397b773c1f872e8067aa9a717919ade7beb4a66b
Aditya Desai <115383821+aditya-desai-19@users.noreply.github.com>
2023-05-11 23:07:29 +0530

    Started with assignment

---
+++ app/src/main/java/com/example/assignment_1/Activities/Technologies.kt
@@ -0,0 +1,12 @@
+package com.example.assignment_1.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.assignment_1.R
+
+class Technologies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_technologies)
+    }
+}
